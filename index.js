808e088991eaeaed61dd771c6684501480307ead
Bradford-Meziere <coleokc@gmail.com>
2021-06-10 17:53:02 -0500

    finished task 2

--- index.js
+++ index.js
@@ -48,10 +48,9 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  // discount: function()
+  discount: function(role){return (role === 'teacher' || role === 'student' )? this.price * 0.75 : this.price * 0.9}
 }
-
-
+console.log('task 2', burger.discount('teacher'))
 
 ///////////////Reviews (MVP)///////////////////
 const reviews = [
@@ -97,6 +96,17 @@ console.log('Task 5', reviews)
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
 
+// const reviews = [
+//     {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
+//     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
+//     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
+//     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+//     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
+//     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+//     {name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
+//     {name: "Reyna", rating: 3.5, feedback: ""},
+// ]
+
 Use the getReviewByIndex function below to do the following:
   1. Receive an array
   2. Receive a number which is the desired index in the array
@@ -105,10 +115,12 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
-}
+function getReviewByIndex(reviews, i=0) {
 
+return `${reviews[i]['name']} gave the restaurant a ${reviews[i]['rating']} star review, and their feedback was: ${reviews[i]['feedback']}`
+} 
+
+console.log('Task 6', getReviewByIndex(reviews))
 
   
 
@@ -124,7 +136,7 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
+function getLastReview(array) {
   /*Your code here*/
 } 
 
